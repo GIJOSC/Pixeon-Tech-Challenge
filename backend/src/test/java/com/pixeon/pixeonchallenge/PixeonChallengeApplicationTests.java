8fc8f7fb1063d5511f5f21a9f2a367e0c4519b51
Gesiel Neves <neves.gesiel@gmail.com>
2022-07-21 14:59:06 -0300

    Project created

---
+++ backend/src/test/java/com/pixeon/pixeonchallenge/PixeonChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pixeon.pixeonchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixeonChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
